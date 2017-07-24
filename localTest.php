910a950f6044f87fb4a2f4366df91a3a7e194ebc
Aaron Retteen <writedoc@gmail.com>
2017-07-24 17:42:39 -0400

    Delete localTest.php

--- localTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-$search_term = "HD052120[Grant Number]";
-$combined_search = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax=1000&tool=FSU_IR&email=aretteen@fsu.edu&term={$search_term}";
-
-print $combined_search;
-?>
